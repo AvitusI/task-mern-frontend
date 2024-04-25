58c3cbc97aedd47d88baf215035cc10e21f013c3
Avitus Ibrahim <avitussweetbert213@gmail.com>
2024-04-26 01:00:33 +0300

    Backend integrated

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,33 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = 'http://localhost:8000/api/users/'
+const API_URL = "https://taskmern-hcj36vpk.b4a.run/api/users/";
 
 const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
 
-    const response = await axios.post(API_URL, userData)
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+  return response.data;
+};
 
-    return response.data
-
-}
-
-const logout = () => localStorage.removeItem('user')
+const logout = () => localStorage.removeItem("user");
 
 const login = async (userData) => {
-    
-    const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post(API_URL + "login", userData);
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-    return response.data
+  return response.data;
+};
 
-}
+const authService = { register, logout, login };
 
-const authService = { register, logout, login }
-
-export default authService
+export default authService;
--- src/features/tasks/taskService.js
+++ src/features/tasks/taskService.js
@@ -1,49 +1,43 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8000/api/tasks/';
-
+const API_URL = "https://taskmern-hcj36vpk.b4a.run/api/tasks/";
 
 const createTask = async (taskData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+  const response = await axios.post(API_URL, taskData, config);
 
-    const response = await axios.post(API_URL, taskData, config)
-
-    return response.data
-
-}
+  return response.data;
+};
 
 const getTasks = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+  const response = await axios.get(API_URL, config);
 
-    const response = await axios.get(API_URL, config)
-
-    return response.data
-
-}
+  return response.data;
+};
 
 const deleteTask = async (id, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+  const response = await axios.delete(API_URL + id, config);
 
-    const response = await axios.delete(API_URL + id, config)
+  return response.data;
+};
 
-    return response.data
-}
+const taskService = { createTask, getTasks, deleteTask };
 
-const taskService = { createTask, getTasks, deleteTask }
-
-export default taskService
+export default taskService;
